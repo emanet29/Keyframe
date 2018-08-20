4e8e4902a35473266d730f0a2b3f71a23fd9943c
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-08-20 14:05:44 +0200

    last commit

--- Keyframe/ViewController.swift
+++ Keyframe/ViewController.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var bouton: UIButton!
+    
+    var frameDeMonBouton: CGRect!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
+    @IBAction func boutonAction(_ sender: Any) {
+        bouton.isUserInteractionEnabled = false
+        frameDeMonBouton = bouton.frame
+        
+        UIView.animateKeyframes(withDuration: 4, delay: 0, options: UIViewKeyframeAnimationOptions.calculationModeLinear, animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25, animations: {
+                self.bouton.backgroundColor = .blue
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.5, animations: {
+                self.bouton.center.y = 50
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.25, animations: {
+                self.bouton.layer.cornerRadius = 25
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25, animations: {
+                self.bouton.alpha = 0
+            })
+        }) { (success) in
+            UIView.animate(withDuration: 1, animations: {
+                self.bouton.backgroundColor = .white
+                self.bouton.frame = self.frameDeMonBouton
+                self.bouton.layer.cornerRadius = 0
+                self.bouton.alpha = 1
+                self.bouton.isUserInteractionEnabled = true
+            })
+        }
+        
+    }
+    
 }
 
